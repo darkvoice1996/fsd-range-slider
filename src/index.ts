d414225599645916cc1ffc080dc1de70b8c207f8
Alexandr Spiridonov <darkvoice1996@gmail.com>
2021-05-19 20:37:53 +0300

    Cleaned old code

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,11 @@ import jQuery from 'jquery'
 
 (function( $ ) {
 
-    let methods = {
-        createInput() {
-            return `<input type="range" class="rs-input" name="" id="" min="0" max="100" value="25">`
-        }
-    }
+
 
     $.fn.fsdRangeSlider = function(options) {
 
-        interface ISettings {
-            min: number
-            max: number
-            isInterval: boolean
-        }
-   
-        let settings : ISettings = $.extend({
-            min: 0,
-            max: 100,
 
-            isInterval: true,
-
-        }, options)
-
-        return this.each(function () {
-            
-        })
   
     };
   })(jQuery);
